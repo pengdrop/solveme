5f60400bcf4dbca7d28c18f2e9d722ce4afe0d78
Safflower <fuzzer@nate.com>
2018-04-08 04:23:21 +0900

    Update header.php

--- @import/header.php
+++ @import/header.php
@@ -15,24 +15,24 @@
 	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta property="description" content="<?php echo __SITE__['description']; ?>">
-		<meta name="keywords" content="<?php echo __SITE__['keyword']; ?>">
+		<meta property="description" content="<?php echo secure_escape(__SITE__['description']); ?>">
+		<meta name="keywords" content="<?php echo secure_escape(__SITE__['keyword']); ?>">
 		<meta name="author" content="Safflower">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta property="og:title" content="<?php echo $title; ?>">
-		<meta property="og:site_name" content="<?php echo __SITE__['title']; ?>">
+		<meta property="og:title" content="<?php echo secure_escape($title); ?>">
+		<meta property="og:site_name" content="<?php echo secure_escape(__SITE__['title']); ?>">
 		<meta property="og:url" content="<?php echo get_url(); ?>">
 		<meta property="og:image" content="<?php echo get_new_cache_link('/assets/img/thumb.png'); ?>">
-		<meta property="og:description" content="<?php echo __SITE__['description']; ?>">
+		<meta property="og:description" content="<?php echo secure_escape(__SITE__['description']); ?>">
 		<meta name="twitter:card" content="summary">
-		<meta name="twitter:title" content="<?php echo $title; ?>">
-		<meta name="twitter:site" content="<?php echo __SITE__['title']; ?>">
+		<meta name="twitter:title" content="<?php echo secure_escape($title); ?>">
+		<meta name="twitter:site" content="<?php echo secure_escape(__SITE__['title']); ?>">
 		<meta name="twitter:url" content="<?php echo get_url(); ?>">
 		<meta name="twitter:image" content="<?php echo get_new_cache_link('/assets/img/thumb.png'); ?>">
-		<meta name="twitter:description" content="<?php echo __SITE__['description']; ?>"/>
+		<meta name="twitter:description" content="<?php echo secure_escape(__SITE__['description']); ?>"/>
 		<link rel="icon" href="<?php echo get_new_cache_link('/assets/img/favicon.png'); ?>">
 		<link rel="apple-touch-icon" href="<?php echo get_new_cache_link('/assets/img/favicon.png'); ?>">
-		<title><?php echo $title; ?></title>
+		<title><?php echo secure_escape($title); ?></title>
 		<!--[if lt IE 9]>
 			<script src="/assets/js/html5shiv.js?v=3.7.0"></script>
 		<![endif]-->
